1502a5e53d7179e2c6cb965aaf52bc50cb976736
Ryan Van Etten <ryanve@gmail.com>
2013-04-03 18:42:52 -0400

    Add files.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-mixin
+### [Mixin](https://github.com/airve/mixin)
 =====
 
-PHP trait for making extensible classes
+PHP [trait](http://php.net/manual/en/language.oop5.traits.php) for making extensible [classes](http://php.net/manual/en/language.oop5.php)
+
+Requires: PHP 5.4+
+
+### License: [MIT](http://opensource.org/licenses/MIT)
+
+Copyright (C) 2013 by [Ryan Van Etten](https://github.com/ryanve)
---
+++ mixin.php
@@ -0,0 +1,149 @@
+<?php 
+/**
+ * @package  airve/mixin
+ * @link     http://github.com/airve/mixin
+ * @author   Ryan Van Etten
+ * @license  MIT
+ */
+
+namespace airve;
+
+trait Mixin {
+
+    public function __toString() {
+        return static::toString($this);
+    }
+
+    public function __call($name, $params) {
+        return static::resolve($name, $params, $this);
+    }
+    
+    public static function __callStatic($name, $params) {
+        return static::resolve($name, $params);
+    }
+
+    /**
+     * @param  mixed   $item
+     * @return string
+     */
+    public static function toString($item) {
+        $item and $item = static::result($item);
+        if (null === $item || \is_scalar($item) || \is_callable([$item, '__toString']))
+            return (string) $item;
+        return \json_encode($item = \is_object($item) ? \get_object_vars($item) : (array) $item);
+    }
+    
+    /**
+     * @param  string|int  $name
+     * @param  array       $params
+     * @param  object      $scope
+     * @return mixed
+     */
+    public static function resolve($name, array $params = null, $scope = null) {
+        if ($fn = static::mixin((string) $name))
+            return static::apply($fn, $scope, $params);
+        if ('_e' === \substr($name, -2))
+            echo static::toString(static::apply(static::method(\substr($name, 0, -2), $scope), null, $params));
+        else \trigger_error(\get_called_class() . " method '$name' is not callable.");
+    }
+    
+    /**
+     * @param  array|object|string|int  $name
+     * @param  callable|string          $fn    
+     * @param  int|bool                 $chain
+     * @return mixed
+     */
+    public static function mixin($name, $fn = 0, $chain = 0) {
+        static $mixins;
+        $mixins = $mixins ?: [[], []];
+        $name and $name = static::result($name);
+        if (\is_scalar($name)) {
+            $chain = (int) $chain;
+            $fn and $mixins[$chain][$name] = $fn;
+            return empty($mixins[$chain][$name])
+                ? null : $mixins[$chain][$name];
+        }
+        $chain = (int) $fn;
+        if ($name)
+            foreach ($name as $k => $v)
+                $v and $mixins[$chain][$k] = $v;
+        return $copy = $mixins[$chain];
+    }
+    
+    /**
+     * @param  mixed  $fn
+     * @return mixed
+     */
+    public static function result($fn) {
+        return $fn instanceof \Closure ? \call_user_func_array(
+            $fn, \array_slice(\func_get_args(), 1)
+        ) : $fn;
+    }
+    
+    /**
+     * @param  callable $fn
+     * @param  object   $scope
+     * @return mixed
+     */
+    public static function call(callable $fn, object $scope = null) {
+        return static::apply($fn, $scope, \array_slice(\func_get_args(), 2));
+    }
+
+    /**
+     * @param  callable $fn
+     * @param  object   $scope
+     * @param  array    $params
+     * @return mixed
+     */
+    public static function apply(callable $fn, object $scope = null, array $params = null) {
+        null !== $scope && $fn instanceof \Closure and $fn = \Closure::bind($fn, $scope, \get_class($scope));
+        return \call_user_func_array($fn, $params ?: []);
+    }
+    
+    /**
+     * @param  callable $fn
+     * @return callable
+     */
+    public static function curry($fn) {
+        $curries = \array_slice(\func_get_args(), 1);
+        return function() use ($fn, $curries) {
+            return \call_user_func_array($fn, \array_merge($curries, \func_get_args()));
+        };
+    }
+    
+    /**
+     * @param  callable $fn
+     * @return callable
+     */
+    public static function partial($fn) {
+        $curries = \array_slice(\func_get_args(), 1);
+        return function() use ($fn, $curries) {
+            $params = \func_get_args();
+            foreach ($curries as $i => $v)
+                $params[$i] = null === $v && \array_key_exists($i, $params) ? $params[$i] : $v;
+            return \call_user_func_array($fn, $params);
+        };
+    }
+    
+    /**
+     * @param  string         $name
+     * @param  string|object  $scope
+     * @return callable
+     */
+    public static function method($name, $scope = null) {
+        return [null === $scope ? \get_called_class() : $scope, $name];
+    }
+    
+    /**
+     * @param  string|object  $object
+     * @return array
+     */
+    public static function methods($object = null) {
+        $result = [];
+        null === $object and $object = \get_called_class();
+        foreach (\get_class_methods($object) as $m)
+            $result[$m] = [$object, $m];
+        return $result;
+    }
+    
+}# trait
